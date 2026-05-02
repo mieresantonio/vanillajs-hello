9f4733c403f91b79d8b9fd81879a35189b0a515b
mieresantonio <mieresantonio@gmail.com>
2026-05-02 03:39:22 +0000

    Proyecto generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
+let quien = ["El Maxi", "El Vicente", "El Antonio", "El Joaquin"];
+let accion = ["mordio", "aplasto", "se comio", "orino", "me robo"];
+let queCosa = ["la tarea", "mi celular", "mi auto", "mis zapatos", "mi cuaderno"];
+let cuando = ["en la mañana" , "mientras caminaba", "mientras dormia", "hace 2 meses", "anoche"] ;
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function GeneradorExcusas(){
+
+    let resQuien = quien[Math.floor(Math.random() * quien.length)];
+    let resAccion = accion[Math.floor(Math.random() * accion.length)];
+    let resQueCosa = queCosa[Math.floor(Math.random() * queCosa.length)];
+    let resCuando = cuando[Math.floor(Math.random() * cuando.length)];
+
+    let excusa = resQuien + " " + resAccion + " " + resQueCosa + " " + resCuando;
+
+    document.getElementById("excusa").innerHTML = excusa;
+
+}
+
+
+
+GeneradorExcusas();
+
+setInterval(GeneradorExcusas, 3000)
